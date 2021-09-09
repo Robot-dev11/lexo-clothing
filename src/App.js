f74456f9e0c51fdec09e9c97df9b442ff345f8d5
Giriraj Soni <61830554+Robot-dev11@users.noreply.github.com>
2021-09-09 22:40:21 +0530

    added logo, shop, contact link to the header component and done some changes in App.css file

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import { Switch,Route } from 'react-router-dom';
 import ShopPage from './pages/shop/shop.component';
+import Header from './component/header/header.component.jsx';
 
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Switch>
         <Route exact path='/'><HomePage /></Route>
         <Route path='/shop'><ShopPage /></Route>
