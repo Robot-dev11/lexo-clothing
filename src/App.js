01a47d24939773dde62bf1e89d92201ba8c69aa1
Giriraj Soni <61830554+Robot-dev11@users.noreply.github.com>
2021-09-09 16:38:28 +0530

    update meu-items to navigate with Router HOC

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+import { Switch,Route } from 'react-router-dom';
 
+const HatsPage = () => (
+  <div><h1>HATS PAGE</h1></div>
+);
 function App() {
   return (
     <div className="App">
-      <HomePage />
+      <Switch>
+        <Route exact path='/'><HomePage /></Route>
+        <Route path='/hats'><HatsPage /></Route>
+      </Switch>
     </div>
   );
 }
