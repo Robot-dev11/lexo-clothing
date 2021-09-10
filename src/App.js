970080c84b33382772e87cf92b462a2f021f0e34
Giriraj Soni <61830554+Robot-dev11@users.noreply.github.com>
2021-09-10 18:34:47 +0530

    added new signup page, sign-in and FormInput Component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import HomePage from './pages/homepage/homepage.component';
 import { Switch,Route } from 'react-router-dom';
 import ShopPage from './pages/shop/shop.component';
 import Header from './component/header/header.component.jsx';
+import SignInSignUp from './pages/signin-and-signup/signin-and-signup.component';
 
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <Switch>
         <Route exact path='/'><HomePage /></Route>
         <Route path='/shop'><ShopPage /></Route>
+        <Route path='/signin'><SignInSignUp /></Route>
       </Switch>
     </div>
   );
---
+++ src/pages/signin-and-signup/signin-and-signup.component.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import './signin-and-signup.styles.scss';
+import SignIn from '../../component/sign-in/signin.component';
+
+const SignInSignUp = () => (
+    <div className='signin'><SignIn /></div>
+)
+
+export default SignInSignUp;
